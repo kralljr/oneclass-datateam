efeab9c8e5c633fb65b57a72c3fb1dc90b3def79
kralljr <jenna.krall@gmail.com>
2015-08-02 18:41:09 -0400

    Remove dplyr, fix commenting

--- data-samplelabel.R
+++ data-samplelabel.R
@@ -1,9 +1,6 @@
 # Read in data
 dat <- read.table("fv.uniq.txt", sep = ",")
 
-# Load libraries
-library(dplyr)
-
 # Get linear funciton of features
 linfun <- 2 + 5 * dat[, 1]/ dat[, 2] +	exp(dat[, 5] * dat[, 4]) + 2.5 * dat[, 3]
 
@@ -22,6 +19,6 @@ y <- factor(y, levels = c(0, 1), labels = c("Attack", "Normal"))
 table(y) / length(y)
 
 # Test
-#dat1 <- data.frame(y, dat)
+dat1 <- data.frame(y, dat)
 #glm1 <- glm(y ~ V1 + V2 + V3 + V4 + V5, data = dat1, family = "binomial")
 #summary(glm1)$coef %>% round(., 2)
