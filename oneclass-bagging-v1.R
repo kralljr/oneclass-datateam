403cf12acab8261403c17000451afdfd90e5f37b
kralljr <jenna.krall@gmail.com>
2015-08-02 19:15:04 -0400

    Try to fix bsvm

--- oneclass-bagging-v1.R
+++ oneclass-bagging-v1.R
@@ -2,6 +2,12 @@ library(caret)
 library(e1071)
 library(dplyr)
 
+
+# get data
+source("data-samplelabel.R")
+
+
+
 #Call original svmBag functions
 #str(svmBag)
 #svmBag$fit
@@ -9,11 +15,12 @@ library(dplyr)
 #svmBag$aggregate
 
 #Define custom bagging svm function using svm package of our choosing {fit, pred, aggregate}
-bsvm.fit <- function (x, y, ...)
+bsvm.fit <- function (dat, ...)
      {
+	x <- dat[,-1]
+	y <- dat[, 1]
          loadNamespace("e1071")
-         out <- e1071::svm(as.matrix(x), y, prob.model = is.factor(y),
-             ...)
+         out <- e1071::svm(as.matrix(x), y, kernel = "radial", scale = F, type = "one-classification")
          out
      }
 
@@ -33,20 +40,21 @@ bagctrl <- bagControl(fit = bsvm.fit,
                       aggregate = bsvm.aggregate)
 
 #Fix data formatting
-dat1$y <- as.numeric(dat1$y)
+dat1$y <- as.factor(dat1$y)
 dat1$V4 <- as.factor(dat1$V4)
 dat1$V5 <- as.factor(dat1$V5)
+
 
 #Create a sample subset for faster testing runtimes
 set.seed(300)
 dat1_subset10 <- sample_n(dat1, 1000)
-
+rownames(dat1_subset10) <- NULL
 head(dat1_subset10)
 
 
 #Evaluate bagged function
 set.seed(300)
-svmbag <- train(y ~., data = dat1_subset10, "bag", trControl = ctrl, bagControl = bagctrl)
+svmbag <- train(y ~ ., data = dat1_subset10, method = "bag", trControl = ctrl, bagControl = bagctrl)
 
 #Warnings 11, 22, and 23 mean...?
 warnings()
