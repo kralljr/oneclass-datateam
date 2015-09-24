032e2e9de19fc2e48a9d05368d8af45d59059990
kralljr <jenna.krall@gmail.com>
2015-09-24 14:15:23 -0400

    Add functions to fully time

--- time-full-valid.R
+++ time-full-valid.R
@@ -3,6 +3,12 @@ library(e1071)
 library(dplyr)
 
 
+# Source error function
+source("bag-fun.R")
+source("error-fun.R")
+
+# load means
+load("mean-sd-all.RData")
 
 
 ## Check training time
@@ -11,7 +17,7 @@ library(dplyr)
 lf <- list.files()
 lf <- lf[substr(lf, 1, 6) == "traini"]
 
-dat1 <- read.csv(lf[1], stringsAsFactors = F)
+dat1 <- read.csv(lf[1], header = F, stringsAsFactors = F)
 dat2 <- dat1[, -c(1, 2)]
 
 
@@ -19,6 +25,13 @@ dat2 <- dat1[, -c(1, 2)]
 gam1 <- 3
 nu1 <- .9
 cost1 <- 1
+
+
+
+# columns to remove (no variability)
+rms <- c("V57","V58","V59","V61","V63","V64","V65","V67","V69","V70","V73","V74","V75","V77","V79","V80","V81","V83","V85","V86","V89","V90","V91","V93","V95","V96","V97","V99","V101","V102")
+
+
 
 
 ns <- c(100, 1000, 10000)
@@ -29,19 +42,11 @@ for(i in 1 : length(ns)) {
   datsub <- dat2[seq1, ]
   
   # mean 0
-  mn1 <- colMeans(datsub)
-  datsub <- sweep(datsub, 2, mn1, "-")
+  datsub <- sweep(datsub, 2, mns1, "-")
+  sdT <- sds1 != 0
+  datsub[, sdT] <- sweep(datsub[, sdT], 2, sds1[sdT], "/")
 
-  
-  sd1 <- apply(datsub, 2, sd)
-  wh1 <- which(sd1 == 0) 
-  if(length(wh1) > 0) {
-    datsub[, -wh1] <- sweep(datsub[, -wh1], 2, sd1[-wh1], "/")
-
-  }else{
-
-  }
-
+  datsub <- datsub[, -which(colnames(datsub) %in% rms)]
   
   
   x <- proc.time()
@@ -58,40 +63,52 @@ for(i in 1 : length(ns)) {
 
 
 
-
-## Check validation size
-
-valid1 <- read.csv("validation.csv", stringsAsFactors = F)
-
-
-
-valid2 <- valid1[, -c(1, 2)]
-
-validx <- dplyr::select(valid2, -label)
-
-mn1 <- apply(validx, 2, mean)
-validx <- sweep(validx, 2, mn1, "-")
-sd1 <- apply(validx, 2, sd)
-sd1[sd1 == 0] <- 1
-validx <- sweep(validx, 2, sd1, "/")
-
-
-validy <- valid2$label
-
-
-
+######
+# fix training data
 
 
 # Time 1 tune on N samples
-ef <- NULL
+ef <- mixtype
 tc <- tune.control(sampling = "fix", error.fun = ef)
 
 datsub2 <- mutate(datsub, label = T)
 
 n1 <- ncol(datsub2) - 1
-colnames(datsub2)[-which(colnames(datsub2) == "label")] <- paste0("V", seq(1, n1))
-colnames(validx) <- paste0("V", seq(1, n1))
+colnames(datsub2)[-which(colnames(datsub2) == "label")] <- paste0("col", seq(1, n1))
 
+
+
+
+
+######
+# fix validation data
+## Check validation size
+
+valid1 <- read.csv("validation.csv", stringsAsFactors = F, colClasses = c(rep("numeric", 318), "logical"))
+
+
+
+validx <- dplyr::select(valid1, -label)
+validy <- valid1$label
+colnames(validx) <- paste0("col", seq(1, n1))
+
+# test with subset of validation data
+n2 <- 10000 
+validx <- validx[1 : n2, ]
+validy <- validy[1 : n2]
+
+
+
+
+
+
+
+
+
+
+# add more than 1 parameter
+nu1 <- c(0.5, 0.9)
+gam1 <- c(0.1, 3)
 
 x <- proc.time()
 tune1 <- tune.svm(label ~ ., data = datsub2, validation.x = validx,
@@ -100,3 +117,25 @@ tune1 <- tune.svm(label ~ ., data = datsub2, validation.x = validx,
                   tunecontrol = tc)
 y <- proc.time()
 print(y - x)
+
+
+
+
+# Try with svm, apply function
+colnames(datsub) <- paste0("col", seq(1, ncol(datsub)))
+
+x <- proc.time()
+
+svm_model <- svm(datsub, kernel = "radial", scale = F, nu = nu1[2], gam = gam1[2], type = "one-classification")
+
+p1 <- predict(svm_model, validx)
+mt <- mixtype(validy, p1)
+
+
+y <- proc.time()
+print(y - x)
+
+
+
+
+
