ea147504e1a574da86ff3b1fb1639573aabd116b
kralljr <jenna.krall@gmail.com>
2015-09-25 21:36:03 -0400

    Add second test, pass hyperpar

--- test-mapr.R
+++ test-mapr.R
@@ -20,9 +20,26 @@ source(file.path(dir1, "readNumberedLines.R"))
 tf1 <- "~/Documents/strand/data/train.csv"
 vf1 <- "~/Documents/strand/data/validation.csv"
 
+hyperparm1 <- list()
+hyperparm1$gamma <- c(0.1)
+hyperparm1$nu <- c(0.1)
+
 x <- proc.time()
-svm1 <-   svmboot.mr(tf1, vf1, numboot = 1, sizeboot = 1000)
+Rprof()
+svm1 <-   svmboot.mr(tf1, vf1, numboot = 1, sizeboot = 10000, hyperparm = hyperparm1)
+Rprof(NULL)
+summaryRprof()
 y <- proc.time()
 
 
 #15.5+ (depend on param) hours for 100 hyperparam combos for 10000 bootstrapped samples
+
+
+
+x <- proc.time()
+Rprof()
+svm1 <-   svmboot.map("temp", 1, tf1, vf1, sizeboot = 10000, hyperparm = hyperparm1)
+Rprof(NULL)
+summaryRprof()
+y <- proc.time()
+
