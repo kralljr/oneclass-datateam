0a45878a9653cf0c35f42056c02af94bdacb7f07
kralljr <jenna.krall@gmail.com>
2015-09-30 20:28:17 -0400

    Add test data

--- test-perf.R
+++ test-perf.R
@@ -12,28 +12,36 @@ source(file.path(dir2, "error-fun.R"))
 
 set.seed(10)
 nrow1 <- 1249321
+N <- 10000
 samps <- sample(seq(2, nrow1), 10000)
 dat <- read.csv(textConnection(readNumberedLines("train.csv", samps)), colClasses = "numeric", header = F)
 
 
 
 # read validation data
-valid1 <- read.csv("validation-small.csv")
+#valid1 <- read.csv("validation-small.csv")
 
-validx <- valid1[, -which(colnames(valid1) == "label")]
-validy <- valid1$label
-
-
+#validx <- valid1[, -which(colnames(valid1) == "label")]
+#validy <- valid1$label
 
 
+# read some test data
+set.seed(10)
+nrow1 <- 1249321
+N <- 100000
+test <- read.csv(textConnection(readNumberedLines("test.csv", samps)), colClasses = c(rep("numeric", 318), "logical"), header = F)
 
-gam1 <- .6
-nu1 <- 0.1
+testx <- test[, -319]
+testy <- test[, 319]
+
+
+gam1 <- 3
+nu1 <- .9
 
 # fit SVM on params in gam1, nu1 using dat, validx, validy
 svm_model <- svm(dat, kernel = "radial", scale = F, type = "one-classification", gamma = gam1, nu = nu1)
 
-pred1 <- predict(svm_model, validx)
+pred1 <- predict(svm_model, testx)
 
-type1(validy, pred1)
-type2(validy, pred1)
+type1(testy, pred1)
+type2(testy, pred1)
